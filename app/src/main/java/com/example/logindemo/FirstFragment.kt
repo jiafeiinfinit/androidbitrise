61efe9bce2c4e1c490b3c4514646350d29f77c62
jiafei <jiafei@infinit.com.sg>
2022-01-25 23:15:24 +0800

    init

---
+++ app/src/main/java/com/example/logindemo/FirstFragment.kt
@@ -0,0 +1,52 @@
+package com.example.logindemo
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.logindemo.databinding.FragmentFirstBinding
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class FirstFragment : Fragment() {
+
+    private var _binding: FragmentFirstBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonLogin.setOnClickListener {
+            if("admin".equals(binding.editUsername.text.toString()) && "111111".equals(binding.editPassword.text.toString()) ){
+                binding.errMsg.text = "Login ok"
+                binding.errMsg.setTextColor(Color.GREEN)
+            }else{
+                binding.errMsg.text = "User name or password is not correct"
+                binding.errMsg.setTextColor(Color.RED)
+            }
+            //findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
